f393a0c21f026a3b757c338ddba4b2167ea2bc45
ChaseMilligan <chasemilligan22@gmail.com>
2021-05-09 17:48:13 -0600

    assets n shit boys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public float speed = 10f;
+
+    Vector2 lastClickedPos;
+
+    bool moving;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+
+        if (moving && (Vector2)transform.position != lastClickedPos) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        } else {
+            moving = false;
+        }
     }
 }

6ca7d243851c4321d89f3e5396ef29dd5e905211
ChaseMilligan <chasemilligan22@gmail.com>
2021-05-10 16:13:39 -0600

    made better map and added collisions with player

--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -10,6 +10,8 @@ public class TopDownCharacterController : MonoBehaviour
 
     public Animator animator;
 
+    private int facingDirection = 2;
+
     Vector2 movement;
 
     void Update() {
@@ -18,9 +20,24 @@ public class TopDownCharacterController : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x > 0 && movement.y == 0) {
+            facingDirection = 1;
+        } else if (movement.x < 0 && movement.y == 0) {
+            facingDirection = 3;
+        }
+
+        if (movement.y > 0 && movement.x == 0) {
+            facingDirection = 0;
+        } else if (movement.y < 0 && movement.x == 0) {
+            facingDirection = 2;
+        }
+
+        animator.SetInteger("Facing", facingDirection);
     }
 
     void FixedUpdate() {
+        
         rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }

b821ade732d3f8d6a8ebf9a6cb7029b37c80b5e9
ChaseMilligan <chasemilligan22@gmail.com>
2021-05-09 23:03:25 -0600

    movement and animations for player

---
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        public float speed;
+
+        public Animator animator;
+
+        private void Update() {
+
+            Vector2 dir = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.A)) {
+                dir.x = -1;
+                animator.SetBool("Moving", true);
+                animator.SetFloat("Direction", 3);
+            } else {
+                animator.SetBool("Moving", false);
+            }
+
+            if (Input.GetKey(KeyCode.D)) {
+                dir.x = 1;
+                animator.SetBool("Moving", true);
+                animator.SetFloat("Direction", 1);
+            } else {
+                animator.SetBool("Moving", false);
+            }
+
+            if (Input.GetKey(KeyCode.W)) {
+                dir.y = 1;
+                animator.SetBool("Moving", true);
+                animator.SetFloat("Direction", 0);
+            } else {
+                animator.SetBool("Moving", false);
+            }
+
+            if (Input.GetKey(KeyCode.S)) {
+                dir.y = -1;
+                animator.SetBool("Moving", true);
+                animator.SetFloat("Direction", 2);
+            } else {
+                animator.SetBool("Moving", false);
+            }
+
+            dir.Normalize();
+
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
+        }
+    }
+}

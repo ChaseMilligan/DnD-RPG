3f0a6e7ea30da04452c5df876f6231b1b450df11
Troy Rosin <marksteel23@gmail.com>
2021-05-09 18:00:53 -0600

    added the optoins menu and back button functionality

--- Assets/MAINMENU.cs
+++ Assets/MAINMENU.cs
@@ -9,4 +9,9 @@ public class MAINMENU : MonoBehaviour {
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
    
+   public void QuitGame ()
+    {
+       Debug.Log("QUIT!");
+       Application.Quit();
+    }
 }
